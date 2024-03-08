0e1a3653c4a64f56eb92ede9340fe46371abff1d
守夜人 <1229248506@qq.com>
2024-03-08 16:04:50 +0800

    feat: 添加nginx的gzip压缩

--- website/src/components/Layout.vue
+++ website/src/components/Layout.vue
@@ -1,8 +1,8 @@
 <template>
-    <button @click="home">home</button>
-    <button @click="about">about</button>
-    <p>可访问后端添加数据 <a href="http://127.0.0.1:8000/add">http://127.0.0.1:8000/add</a></p>
-    <RouterView />
+  <el-button type="primary" @click="home">home</el-button>
+  <el-button type="primary" @click="about">about</el-button>
+  <p>可访问后端添加数据 <a href="http://127.0.0.1:8000/add">http://127.0.0.1:8000/add</a></p>
+  <RouterView />
 </template>
 
 <script setup lang="ts">
@@ -10,9 +10,9 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 
 function home() {
-    router.push('/home')
+  router.push('/home')
 }
 function about() {
-    router.push('/about')
+  router.push('/about')
 }
 </script>
